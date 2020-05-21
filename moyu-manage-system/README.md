ab874ec604385619a67a2d22b9fb58fd3e5ddfe3
段牧知 <17373217@buaa.edu.cn>
2020-05-21 20:05:53 +0800

    moyu web front-end code

---
+++ moyu-manage-system/README.md
@@ -0,0 +1,26 @@
+# moyu-manage-system
+
+## 安装步骤
+
+```
+// 先把模板下载到本地
+
+cd moyu-manage-system    // 进入模板目录
+npm install         // 安装项目依赖，等待安装完成之后，安装失败可用 cnpm 或 yarn
+
+// 开启服务器，浏览器访问 http://localhost:8080
+npm run serve
+
+// 执行构建命令，生成的dist文件夹放在服务器下即可访问
+npm run build
+```
+
+效果图
+
+![1590052257305](C:\Users\Duanmz\AppData\Roaming\Typora\typora-user-images\1590052257305.png)
+
+![1590052262152](C:\Users\Duanmz\AppData\Roaming\Typora\typora-user-images\1590052262152.png)
+
+![1590052267764](C:\Users\Duanmz\AppData\Roaming\Typora\typora-user-images\1590052267764.png)
+
+![1590052272194](C:\Users\Duanmz\AppData\Roaming\Typora\typora-user-images\1590052272194.png)
---
+++ moyu-manage-system/src/components/common/i18n.js
@@ -0,0 +1,30 @@
+export const messages = {
+    'zh': {
+        i18n: {
+            breadcrumb: '作者团队介绍',
+            tips: '通过切换语言按钮，来改变当前内容的语言。',
+            btn: '切换英文',
+            title1: '团队信息介绍',
+            p1: '队名：墨玉（201组）',
+            p2: '组长：陆广炎',
+            p3: '队员：赵子敬、段牧知、陈宇航、陈宇轩。',
+            title2: '作品信息介绍',
+            info: '墨玉机器人为一款基于启智ROS开发平台设计的，可以灵活行进，识别目标物体并抓取目标物体的机器人。我们期望他能用于酒店服务，主要工作场景是房间送物和引导带路。',
+            value: '文档'
+        }
+    },
+    'en': {
+        i18n: {
+            breadcrumb: 'International Products',
+            tips: 'Click on the button to change the current language. ',
+            btn: 'Switch Chinese',
+            title1: 'Common usage',
+            p1: "If you reveal your secrets to the wind you should not blame the wind for  revealing them to the trees.",
+            p2: "Nothing can help us endure dark times better than our faith. ",
+            p3: "If you can do what you do best and be happy, you're further along in life  than most people.",
+            title2: 'Component interpolation',
+            info: 'The default language of Element is Chinese. If you wish to use another language, please refer to the {action}.',
+            value: 'documentation'
+        }
+    }
+}
