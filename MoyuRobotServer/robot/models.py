00c74a5c230d9a6ba3eb16934344f0420a2ed9bd
Nemo <429564633@qq.com>
2020-05-27 13:23:04 +0800

    add database for django

--- MoyuRobotServer/robot/models.py
+++ MoyuRobotServer/robot/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    roomNo = models.CharField(max_length=50, primary_key=True)
+
+class Robot(models.Model):
+    robotNo = models.CharField(max_length=50, primary_key=True)
+    robotState = models.IntegerField()
+    robotIdx_x = models.FloatField()
+    robotIdx_y = models.FloatField()
+    robotPower = models.IntegerField()
+
+class Goods(models.Model):
+    goodsNo = models.CharField(max_length=50, primary_key=True)
+    goodsName = models.CharField(max_length=50)
+    goodsIdx_x = models.FloatField()
+    goodsIdx_y = models.FloatField()
+
+class Instruction(models.Model):
+    insNo = models.CharField(max_length=50, primary_key=True)
+    insType = models.IntegerField()
+    insRoomNo = models.ForeignKey(Room, on_delete=models.CASCADE)
+    insGoodsNo = models.ForeignKey(Goods, on_delete=models.CASCADE)
