44a5b75ebd520370175348b28ca90497383719bf
Nemo <429564633@qq.com>
2020-06-01 22:46:07 +0800

    添加供前端调用的导航、自主取物API框架

--- MoyuRobotServer/robot/urls.py
+++ MoyuRobotServer/robot/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('navigate/', views.navigate, name='navigate'),
+    path('fetch_item/', views.fetch_item, name='fetch_item')
 ]
--- MoyuRobotServer/robot/views.py
+++ MoyuRobotServer/robot/views.py
@@ -1,2 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from django.core.handlers.wsgi import WSGIRequest
+
+def navigate(request: WSGIRequest):
+    pass
+
+def fetch_item(request: WSGIRequest):
+    pass
