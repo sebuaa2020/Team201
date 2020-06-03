e6113511b77347bc721fed28cc2b8d5b4d895622
Nemo <429564633@qq.com>
2020-06-03 10:37:46 +0800

    fix bug

--- MoyuRobotServer/manage.py
+++ MoyuRobotServer/manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MoyuRobotServer.settings')
+    rospy.init_node('commonder', anonymous=True)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
