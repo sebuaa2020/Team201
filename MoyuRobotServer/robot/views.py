2ab8687b8f31654480404cb712569f445caf90ab
Nemo <429564633@qq.com>
2020-06-03 11:06:59 +0800

    添加navigate接口

--- MoyuRobotServer/robot/views.py
+++ MoyuRobotServer/robot/views.py
@@ -8,7 +8,42 @@ from django.core.handlers.wsgi import WSGIRequest
 from .models import Room
 
 def navigate(request: WSGIRequest):
-    pass
+    paras = request.GET
+    source_x = paras['source_x']
+    source_y = paras['source_y']
+    target_x = paras['target_x']
+    target_y = paras['target_y']
+    move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+    move_base.wait_for_server()
+    goal = MoveBaseGoal()  
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.pose.position.x = source_x
+    goal.target_pose.pose.position.y = source_y
+    goal.target_pose.pose.orientation.w = 1.0
+    move_base.send_goal(goal)
+    wait = move_base.wait_for_result(rospy.Duration(120))
+    if not wait:
+        response = {
+            'message': 'Failed!',
+            'success': False
+        }
+        response = json.dumps(response)
+        return HttpResponse(response)
+
+    goal = MoveBaseGoal()  
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.pose.position.x = target_x
+    goal.target_pose.pose.position.y = target_y
+    goal.target_pose.pose.orientation.w = 1.0
+    move_base.send_goal(goal)
+    response = {
+        'message': 'Succeed!',
+        'success': True
+    }
+    response = json.dumps(response)
+    return HttpResponse(response)
 
 def deliver(request: WSGIRequest):
     paras = request.GET
